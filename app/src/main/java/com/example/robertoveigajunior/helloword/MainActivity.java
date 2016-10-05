33c2763b8e02a197484d5a9f79ad9f6e1514b278
Roberto Veiga Junior <robertoveigajunior@MacBook-Air-de-Roberto.local>
2016-10-05 11:30:12 -0300

    first commit

---
+++ app/src/main/java/com/example/robertoveigajunior/helloword/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.robertoveigajunior.helloword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(this, R.string.message, Toast.LENGTH_LONG).show();
+    }
+}
